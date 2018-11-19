e4306f6b9fcc7293e2c26cff3432436c54328c7a
studen <student@localhost>
2018-11-20 00:03:27 +0300

    Fixed int bonus and operator if

--- src/main/java/ru/itpark/CashBackService.java
+++ src/main/java/ru/itpark/CashBackService.java
@@ -3,18 +3,17 @@ package ru.itpark;
 public class CashBackService {
     public int calculateCashback(int amountOrdinary, int amountIncreased, int amountSpecial) {
         int cashbackLimit = 3000;
-        int bonus;
         int cashbackPercentOrdinary = 1;
         int cashbackPercentIncreased = 5;
         int cashbackPercentSpecial = 30;
 
-        bonus = (amountOrdinary * cashbackPercentOrdinary + amountIncreased * cashbackPercentIncreased + amountSpecial * cashbackPercentSpecial) / 100;
+        int bonus = (amountOrdinary * cashbackPercentOrdinary + amountIncreased * cashbackPercentIncreased + amountSpecial * cashbackPercentSpecial) / 100;
 
-        if (bonus < cashbackLimit) {
-            return bonus;
-        } else {
+        if (bonus > cashbackLimit) {
             return cashbackLimit;
         }
+
+        return bonus;
     }
 
 }
